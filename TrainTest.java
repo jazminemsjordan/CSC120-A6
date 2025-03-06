4d0c65e7a0d3b8c36f9a992e8cebeef12bc44b4f
jazminemsjordan <jazminejordan77@gmail.com>
2025-03-06 11:57:41 -0500

    import and begin tests

--- TrainTest.java
+++ TrainTest.java
@@ -1,17 +1,23 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+
 public class TrainTest {
+
+    Engine testEngine = new Engine(FuelType.ELECTRIC, 100, 100);
 
     // Engine Tests
     @Test
     public void testEngineConstructor() {
-        fail();
+        assertEquals(FuelType.ELECTRIC, testEngine.getFuelType());
+        assertEquals(100, testEngine.getCurrentFuel(), 0);
+        assertEquals(100, testEngine.getMaxFuel(), 0);
     }
 
     @Test
     public void testEngineGo() {
-        fail();
+        testEngine.go();
+        assertEquals(testEngine.getMaxFuel() - 10,  testEngine.getCurrentFuel(), 0);
     }
 
     // Car Tests
